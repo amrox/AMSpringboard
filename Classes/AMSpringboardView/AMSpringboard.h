cd3e45b802af6f6a2f04e4fbc1547d7fb170058b
Andy Mroczkowski <andy@mrox.net>
2011-03-29 20:22:55 -0700

    Added shared header and error codes

---
+++ Classes/AMSpringboardView/AMSpringboard.h
@@ -0,0 +1,17 @@
+//
+//  AMSpringboard.h
+//  AMSpringboardView
+//
+//  Created by Andy Mroczkowski on 3/29/11.
+//  Copyright 2011 Andy Mroczkowski. All rights reserved.
+//
+
+#import "AMSpringboardView.h"
+#import "AMSpringboardViewCell.h"
+#import "NSIndexPath+AMSpringboardView.h"
+
+enum AMSpingboardErrorCode {
+    AMSpringboardUnkownPlistFormatError = 1,
+};
+
+#define AMSpringboardErrorDomain @"com.amerzing.AMSpringboard"
