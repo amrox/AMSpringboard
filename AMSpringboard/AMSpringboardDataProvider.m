0df23fb9865273481e63837a4ed609ad439b3a3f
Andy Mroczkowski <andy@mrox.net>
2011-09-24 19:30:18 -0700

    merge fixes

--- AMSpringboard/AMSpringboardDataProvider.m
+++ AMSpringboard/AMSpringboardDataProvider.m
@@ -121,12 +121,6 @@
 {
     NSArray* items = [self.pages objectAtIndex:[position springboardPage]];
     NSUInteger index = [position springboardColumn] + [position springboardRow]*self.columnCount;
-
-    // column major
-//    NSUInteger index = [position springboardColumn]*self.rowCount + [position springboardRow];
-    
-    // row major
-    NSUInteger index = [position springboardColumn] + [position springboardRow]*self.columnCount;
     
     if( index >= [items count] )
         return nil;
